4eb5330918be4560bd13c5e011620363e8746374
AHNAF ALIF TSABITUL 'AZMI DHIYAUL HAQ <ahnafalf05@gmail.com>
2025-11-11 07:48:18 +0700

    first

---
+++ JOBSHEET7/WhileLeaveEntitlement1.java
@@ -0,0 +1,23 @@
+package JOBSHEET7;
+
+import java.util.Scanner;
+
+public class WhileLeaveEntitlement1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int confirmation;
+
+
+do { 
+    System.out.println("Do you want to take a leave (Y/N)? ");
+    confirmation = input.nextInt();
+
+    if(confirmation.equalsIgnoreCase("y")) {
+        
+    }
+
+    
+} while (true);
+    }
+    
+}
