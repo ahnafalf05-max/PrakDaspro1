b3325999056daebc662917c80a3223100e6edf4a
AHNAF ALIF TSABITUL 'AZMI DHIYAUL HAQ <ahnafalf05@gmail.com>
2025-11-11 08:10:00 +0700

    first

--- JOBSHEET10/Cinema1.java
+++ JOBSHEET10/Cinema1.java
@@ -1,10 +1,7 @@
 package JOBSHEET10;
 
-import java.util.Scanner;
-
 public class Cinema1 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
         String [][] audience = new String[4][2];
         audience [0][0] = "Amin";
         audience [0][1] = "Bena";
@@ -13,6 +10,7 @@ public class Cinema1 {
         audience [2][0] = "Eka";
         audience [2][1] = "Farhan";
         audience [3][0] = "Gisel";
+        audience [3][1] = "Hana";
 
 System.out.printf("%s\t %s\n", audience[0][0], audience[0][1]);
 System.out.printf("%s\t %s\n", audience[1][0], audience[1][1]);
---
+++ JOBSHEET10/CinemaWithScanner1.java
@@ -0,0 +1,38 @@
+package JOBSHEET10;
+
+import java.util.Scanner;
+
+public class CinemaWithScanner1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int row;
+        int column;
+        String name;
+        String next;
+
+        String[][] audience = new String[4][2];
+
+        while (true) {
+            System.out.print("Enter a name: ");
+            name = sc.nextLine(); 
+
+            System.out.print("Enter row number: ");
+            row = sc.nextInt();
+
+            System.out.print("Enter column number: ");
+            column = sc.nextInt();
+            sc.nextLine(); 
+
+            audience[row - 1][column - 1] = name;
+
+            System.out.print("Are there any other audiences to be added? (y/n): ");
+            next = sc.nextLine(); 
+
+            if (next.equalsIgnoreCase("n")) {
+                break;
+            }
+        }
+
+        
+        }
+    }
