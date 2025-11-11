bc2050deaf656d1d1363ecee7490484d524f8b3e
AHNAF ALIF TSABITUL 'AZMI DHIYAUL HAQ <ahnafalf05@gmail.com>
2025-11-11 11:06:54 +0700

    FIVE

--- JOBSHEET10/Cinema1.java
+++ JOBSHEET10/Cinema1.java
@@ -13,8 +13,8 @@ public class Cinema1 {
         audience [3][1] = "Hana";
 
         System.out.println(audience.length);
-        for (int i = 0; i < audience.length; i++) {
-            System.out.println("Length of row " + (i + 1) + ": " + audience[i].length);
+        for (String[] rowAudience : audience) {
+            System.out.println("Length of row: " + rowAudience.length);
 
             
         }
@@ -40,5 +40,6 @@ Do audience[0].length, audience[1].length, audience[2].length, and
 audience[3].length have the same value? Why? 
 5. Modify the program code in step 4 to display the length of each row in the array using a for loop. Compile, run, then commit
 DONE
-
+6. Modify the program code in step 5 to display the length of each row in the array using a foreach loop. Compile, run, then commit
+DONE
 */
