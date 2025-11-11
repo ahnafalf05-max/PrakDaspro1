0b4f71ace270d74e952d7a69e714254d2d7db176
AHNAF ALIF TSABITUL 'AZMI DHIYAUL HAQ <ahnafalf05@gmail.com>
2025-11-11 11:46:36 +0700

    DONE

--- JOBSHEET10/Numbers1.java
+++ JOBSHEET10/Numbers1.java
@@ -1,29 +1,38 @@
 package JOBSHEET10;
 
-
 import java.util.Arrays;
-
-
 
 public class Numbers1 {
     public static void main(String[] args) {
-    
-
-        int[][] myNumbers = new int[3][];
-        myNumbers [0] = new int[5];
-        myNumbers [1] = new int[3];
-        myNumbers [2] = new int[1];
-    for (int i = 0; i < myNumbers.length; i++);
-        System.out.println(Arrays.toString(myNumbers[0]));
-    for (int i = 0; i < myNumbers.length; i++) {
-        System.out.println("Length of row " + (i + 1) + ": " + myNumbers[i].length);
 
         
-    }
+        int[][] myNumbers = new int[3][5];
 
-  
+        
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println("Row " + (i + 1) + ": " + Arrays.toString(myNumbers[i]));
+        }
+
+        
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println("Length of row " + (i + 1) + ": " + myNumbers[i].length);
+        }
+
+        
+        int value = 1;
+        for (int i = 0; i < myNumbers.length; i++) {
+            for (int j = 0; j < myNumbers[i].length; j++) {
+                myNumbers[i][j] = value++;
+            }
+        }
+
+        
+        System.out.println("\nUpdated array with values:");
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println("Row " + (i + 1) + ": " + Arrays.toString(myNumbers[i]));
+        }
     }
-    }
+}
     
 
 /* QUESTIONS
@@ -36,5 +45,6 @@ used to convert an array into a readable string format.
 an array of int  without initializing its elements, each element is automatically set to the default value
 4.	Add the following program code: 
 DONE
-
+5.	The myNumbers array has a different length for each row. How to make the length for each row the same? Can the array length be modified? 
+DONE
  */
