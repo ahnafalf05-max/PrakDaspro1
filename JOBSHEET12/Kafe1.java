969bf4f4279ea58e5c69dafcadf0fde8c5add387
AHNAF ALIF TSABITUL 'AZMI DHIYAUL HAQ <ahnafalf05@gmail.com>
2025-11-26 09:28:59 +0700

    two

--- JOBSHEET12/Kafe1.java
+++ JOBSHEET12/Kafe1.java
@@ -1,7 +1,12 @@
 package JOBSHEET12;
 
 public class Kafe1 {
-    public static void Menu() {
+    public static void Menu(String namaPelanggan, boolean isMemeber) {
+        System.out.println("selamat datang, " + namaPelanggan + "!");
+
+        if (isMemeber) {
+            System.out.println("anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        }
         System.out.println("==== MENU RESTO KAFE ====");
         System.out.println("1. kopi hitam - Rp 15,000");
         System.out.println("2. cappuccino - Rp 20,000");
@@ -13,7 +18,7 @@ public class Kafe1 {
         System.out.println("silahkan pilih menu yang anda inginkan.");
     }
 public static void main(String[] args) {
-    Menu();
+    Menu("Andi", true);
     
 }
     
