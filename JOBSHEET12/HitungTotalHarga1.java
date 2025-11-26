0a14cb0157b20fe7b08885f5063de8df4d29b826
AHNAF ALIF TSABITUL 'AZMI DHIYAUL HAQ <ahnafalf05@gmail.com>
2025-11-26 12:37:33 +0700

    four

--- JOBSHEET12/HitungTotalHarga1.java
+++ JOBSHEET12/HitungTotalHarga1.java
@@ -1,5 +1,7 @@
 package JOBSHEET12;
 
+import java.util.Scanner;
+
 public class HitungTotalHarga1 {
 
     public static void Menu() {
@@ -12,8 +14,24 @@ public class HitungTotalHarga1 {
         System.out.println("6. Jus Buah        - Rp18.000");
     }
 
+    public static int hitungTotalHarga(int pilihanMenu, int banyakItem) {
+        int[] hargaItems = {15000, 20000, 22000, 12000, 10000, 18000};
+        return hargaItems[pilihanMenu - 1] * banyakItem;
+    }
+
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
         Menu();
+
+        System.out.print("\nMasukkan nomor menu yang ingin Anda pesan: ");
+        int pilihanMenu = sc.nextInt();
+
+        System.out.print("Masukkan jumlah item yang ingin dipesan: ");
+        int banyakItem = sc.nextInt();
+
+        int totalHarga = hitungTotalHarga(pilihanMenu, banyakItem);
+
+        System.out.println("Total harga untuk pesanan Anda: Rp" + totalHarga);
     }
 }
