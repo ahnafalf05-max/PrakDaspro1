267dea7ed6777ce4494a39e4d8b3068d9b868ba9
AHNAF ALIF TSABITUL 'AZMI DHIYAUL HAQ <ahnafalf05@gmail.com>
2025-11-26 12:33:09 +0700

    three

--- JOBSHEET12/HitungTotalHarga1.java
+++ JOBSHEET12/HitungTotalHarga1.java
@@ -1,11 +1,19 @@
 package JOBSHEET12;
 
 public class HitungTotalHarga1 {
-    public static int HitungTotalHarga(int pilihanMenu, int banyakItem) {
-    int[] hargaItems = {15000, 20000, 22000, 12000, 10000, 18000};
-    
-    int hargaTotal = hargaItems[pilihanMenu - 1] * banyakItem;
-    return hargaTotal;
-}
 
+    public static void Menu() {
+        System.out.println("=== Menu Kafe ===");
+        System.out.println("1. Nasi Goreng     - Rp15.000");
+        System.out.println("2. Ayam Bakar      - Rp20.000");
+        System.out.println("3. Mie Goreng      - Rp22.000");
+        System.out.println("4. Es Teh          - Rp12.000");
+        System.out.println("5. Air Mineral     - Rp10.000");
+        System.out.println("6. Jus Buah        - Rp18.000");
+    }
+
+    public static void main(String[] args) {
+        
+        Menu();
+    }
 }
