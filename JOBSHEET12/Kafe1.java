c72ba1d177f7c9502ead7fd503f99a08f2ce57a9
AHNAF ALIF TSABITUL 'AZMI DHIYAUL HAQ <ahnafalf05@gmail.com>
2025-11-26 08:02:01 +0700

    first

---
+++ JOBSHEET12/Kafe1.java
@@ -0,0 +1,20 @@
+package JOBSHEET12;
+
+public class Kafe1 {
+    public static void Menu() {
+        System.out.println("==== MENU RESTO KAFE ====");
+        System.out.println("1. kopi hitam - Rp 15,000");
+        System.out.println("2. cappuccino - Rp 20,000");
+        System.out.println("3. Latte - Rp 22,000");
+        System.out.println("4. Teh tarik - Rp 12,000");
+        System.out.println("5. roti bakar - Rp 10,000");
+        System.out.println("6. mie goreng - Rp 18,000");
+        System.out.println("===========================");
+        System.out.println("silahkan pilih menu yang anda inginkan.");
+    }
+public static void main(String[] args) {
+    Menu();
+    
+}
+    
+}
