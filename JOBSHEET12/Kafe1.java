bbdbe55ef8415f4f3b75c7a520e262bdf3a30c13
AHNAF ALIF TSABITUL 'AZMI DHIYAUL HAQ <ahnafalf05@gmail.com>
2025-11-26 11:47:59 +0700

    two

--- JOBSHEET12/Kafe1.java
+++ JOBSHEET12/Kafe1.java
@@ -1,18 +1,28 @@
 package JOBSHEET12;
 
 public class Kafe1 {
-    public static void Menu() {
-    System.out.println("==== MENU RESTO KAFE ====");
-    System.out.println("1. Kopi Hitam - Rp 15,000");
-    System.out.println("2. Cappuccino - Rp 20,000");
-    System.out.println("3. Latte - Rp 22,000");
-    System.out.println("4. Teh Tarik - Rp 12,000");
-    System.out.println("5. Roti Bakar - Rp 10,000");
-    System.out.println("6. Mie Goreng - Rp 18,000");
-    System.out.println("Silakan pilih menu yang Anda inginkan.");
+    public static void main(String[] args) {
+        System.out.println("==== MENU RESTO KAFE ====");
+        System.out.printf("1. %s - Rp %,d%n", "Kopi Hitam", 15000);
+        System.out.printf("2. %s - Rp %,d%n", "Cappuccino", 20000);
+        System.out.printf("3. %s - Rp %,d%n", "Latte", 22000);
+        System.out.printf("4. %s - Rp %,d%n", "Teh Tarik", 12000);
+        System.out.printf("5. %s - Rp %,d%n", "Roti Bakar", 10000);
+        System.out.printf("6. %s - Rp %,d%n", "Mie Goreng", 18000);
+    }
 }
-public static void main(String[] args) {
-    Menu();
-}
+/*
+PERCOBAAN 1
+1. Apakah fungsi tanpa parameter selalu harus bertipe void?
+Fungsi tanpa parameter artinya fungsi tersebut tidak menerima input dari luar.
+Namun, fungsi tersebut masih bisa mengembalikan nilai dengan tipe data tertentu
+misalnya int, float, string
+2.Apakah daftar menu pada program kafe dapat ditampilkan tanpa menggunakan fungsi 
+Menu()? Modifikasi kode program tersebut untuk dapat menampilkan daftar menu 
+tanpa menggunakan fungsi!
+Ya, daftar menu bisa ditampilkan tanpa fungsi  dengan menuliskan langsung perintah
 
-}
+
+
+
+*/
